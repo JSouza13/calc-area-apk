b672e44349029c1ba8b2f855ccd95589584d0888
João Vitor de Souza <jooao_souza@hotmail.com>
2019-09-21 22:49:04 -0300

    layout telas

--- joao_vitor_souza/app/src/main/java/com/example/joao_vitor_souza/MainActivity.java
+++ joao_vitor_souza/app/src/main/java/com/example/joao_vitor_souza/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.joao_vitor_souza;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("ALCM", "Método onCreate chamado");
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("ALCM", "Método onStart chamado");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("ALCM", "Método onResume chamado");
+
     }
 }
